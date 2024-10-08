53849e9bb25523ad63e4251af01eb0218598cebe
anil-gehlot <anilgehlot2000@gmail.com>
2024-10-08 15:01:40 +0530

    constant data because no API provided

---
+++ src/utils/constant.js
@@ -0,0 +1,222 @@
+export const articleList = [
+  {
+    title: "China Has the Most Powerful Laser",
+    description:
+      "Chinese scientists have achieved a breakthrough in laser technology with the development of the world's most powerful 'sound laser,' emitting particles of sound instead of light. This groundbreaking device, created by researchers at Hunan Normal University, represents a significant advancement in phonon laser technology, offering potential applications in medical imaging, deep-sea exploration, and various scientific fields.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_limit/v1728253705/user_uploads/ngcwujsrn/china-lazer.jpg",
+    link: "/page/china-has-the-most-powerful-la-18f6BgzXSUyzWSmGigD6CA",
+    author: "elymc",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/831bd9b7-78f7-4d06-7380-44809b816500/thumbnail",
+  },
+  {
+    title: "AI Sees Faces in Objects, Too",
+    description:
+      "Recent research from MIT, awaiting peer review, revealsthat artificial intelligence can now recognize faces ininanimate objects, a phenomenon known as pareidolia,opening new avenues for understanding both machinelearning and human cognition. This capability, while initially challenging for AI, has been significantly improved through training on animal faces, suggesting intriguing links between evolutionary survival skills and our tendency to see faces in everyday objects.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_thumbnail/v1728252855/server_uploads/fwikevz1lhccjdxsonuw.gif",
+    link: "/page/ai-can-recognize-faces-in-obje-ExlxlkLFSYiXLWzJr.zJgw",
+    author: "elymc",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/831bd9b7-78f7-4d06-7380-44809b816500/thumbnail",
+  },
+  {
+    title: "The McLaren Hybrid Hypercar",
+    description:
+      "The McLaren W1, successor to the legendary P1, is a groundbreaking hybrid hypercar that pushes the boundaries of automotive engineering with its 1,258-horsepower powertrain, advanced aerodynamics, and Formula 1-inspired technology. As reported by Car and Driver, this plug-in hybrid marvel combines a twin-turbo 4.0-liter V-8 with an electric motor, delivering blistering performance and upto 2,205 pounds of downforce.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_thumbnail/v1728252175/server_uploads/fyazjfut2miesumbvawi.jpg",
+    link: "/page/the-mclaren-hybrid-hypercar-V.5ID5kBSAy2G53Qg3QXvA",
+    author: "elymc",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/831bd9b7-78f7-4d06-7380-44809b816500/thumbnail",
+  },
+  {
+    title: "Spy Planes Show Radioactive Thunderstorms",
+    description:
+      "Based on reports from Nature, a retrofitted U-2 spy planehas revealed that most tropical thunderstorms are radioactive, producing various forms of gamma radiation far more frequently than previously thought, challenging our understanding of storm physics and atmospheric phenomena.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/fetch/s--9DAtRECa--/t_thumbnail/https://imagenes.elpais.com/resizer/v2/EON4WU2NO5DN3EH6N4NLMVQT4E.jpg%3Fauth%3D36088aa4f2687ddedfdbcd0f49078a361433155cb3ef53fb2e0e32fd7e10057d%26width%3D1200",
+    link: "/page/spy-planes-show-radioactive-th-NR7VerrtSG2tf6KLbIqvWg",
+    author: "elymc",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/831bd9b7-78f7-4d06-7380-44809b816500/thumbnail",
+  },
+  {
+    title: "Keanu's Racing Debut Spin",
+    description:
+      "Actor Keanu Reeves made his professional auto racing debut at Indianapolis Motor Speedway, competing in the Toyota GR Cup series where he qualified 31st, ran as high as 21st, but ultimately finished 25th after spinning out in Turn 9 during the 45-minute race.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_limit/v1728246613/server_uploads/mepywxekoqx4ta74blcb.jpg",
+    link: "/page/keanu-s-racing-debut-spin-SiiV6AnsTbqcP_ZBf48eLQ",
+    author: "urbanxplorer",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/4f64ef5e-122d-48ad-e042-261160b57a00/thumbnail",
+  },
+  {
+    title: "David Lynch's Interview Project",
+    description:
+      "David Lynch's Interview Project, a compelling documentary series directed by Austin Lynch and Jason S, chronicles a 20,000-mile journey across America through 121 short interviews with individuals from small towns and rural areas. Set for an HD re-release on YouTube in summer 2024, this series offers an intimate glimpse into the diverse and unfiltered stories of everyday Americans, capturing the richness of their personal experiences and reflections  on life.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_thumbnail/v1728181218/getty_uploads/870140864.jpg",
+    link: "/page/david-lynchs-interview-project-JDPq6SY4QI.CxaIFcf2WTA",
+    author: "elymc",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/831bd9b7-78f7-4d06-7380-44809b816500/thumbnail",
+  },
+  {
+    title: "Broadcom Joins Magnificent 7",
+    description:
+      "According to reports from the Financial Times and Variety, legendary British rock band Pink Floyd has sold their recorded music catalog, along with name and likeness rights, to Sony Music for approximately $400 million, marking one of the largest such deals in recent years.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_thumbnail/v1728159268/server_uploads/vi1b0lmsv5dxvsel6rjg.jpg",
+    link: "page/rock-and-pink-floyd",
+    author: "stephenhoban",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/6e59b304-1220-44e5-d6e3-ea17b0c8aa00/thumbnail",
+  },
+  {
+    title: "Extinct Biblical Tree Regrown",
+    description:
+      "According to LiveScience, scientists have successfully cultivated a tree from a 1,000-year-old seed discovered inthe Judean Desert, potentially reviving an extinct species linked to Biblical texts and offering promising medicinal properties.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_limit/v1728085119/server_uploads/q3wrjith5m5fmz8axkja.webp",
+    link: "/page/extinct-biblical-tree-regrown-ZzLe6YaOT0qMuUSqhdVgeg",
+    author: "elymc",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/831bd9b7-78f7-4d06-7380-44809b816500/thumbnail",
+  },
+  {
+    title: "US Job Market September Boom",
+    description:
+      "The U.S. job market showed unexpected strength in September, with nonfarm payrolls increasing by 254,000 jobs, significantly surpassing economists' forecasts of 140,000 and potentially altering the Federal Reserve's approach to interest rate cuts. The day before the report, Polymarket users had only placed 4% odds of such a strong report.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_thumbnail/v1728076976/getty_uploads/2175811742.jpg",
+    link: "/page/us-job-market-september-boom-6atM9fH_S9q0eck8yGXwbQ",
+    author: "polymarket",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/5d57bdd7-5f9c-4c94-70b6-3da174bbec00/thumbnail",
+  },
+  {
+    title: "The Cobra Effect",
+    description:
+      "The Cobra Effect, a term coined by German economist HorstSiebert, describes how well-meaning solutions can inadvertently worsen problems, as exemplified by a colonial India anecdote where a bounty on cobras led to increased breeding. This phenomenon illustrates the dangers of oversimplified solutions and misaligned incentives across various fields, from urban planning and business to healthcare and education, highlighting the need for careful policy design and implementation to avoid.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_thumbnail/v1728084774/getty_uploads/1301036947.jpg",
+    link: "/page/the-cobra-effect-Nyd6rlQvTdWw_wlwF0H10w",
+    author: "elymc",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/831bd9b7-78f7-4d06-7380-44809b816500/thumbnail",
+  },
+  {
+    title: "Rivian's Supply Chain Challenges",
+    description:
+      "Rivian, the electric vehicle manufacturer, has experienced notable declines in production and sales during the third quarter of 2024, with production down nearly 24% and deliveries falling by 55% compared to the previous year. According to InsideEVs, these challenges are largely due to ongoing supply chain issues affecting its R1 and Electric Delivery Van platforms, prompting the company to adjust its production and delivery forecasts for the year.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_thumbnail/v1728084228/server_uploads/flufhg12bxk6rso1ytgk.webp",
+    link: "/page/rivian-s-supply-chain-challeng-L799h2A.R_2RE.knjZeb6Q",
+    author: "elymc",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/831bd9b7-78f7-4d06-7380-44809b816500/thumbnail",
+  },
+  {
+    title: "Meta Unveils Movie Gen",
+    description:
+      "Meta has unveiled Movie Gen, a cutting-edge generative AI tool that can create and edit videos and audio using text prompts, marking a significant advancement in AI-powered media creation. As reported by CNET, this new technology allows users to generate high-quality videos, perform personalized video creation, edit existing footage, and produce synchronized audio content.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_limit/v1728079780/server_uploads/i53asemb7bsbxkuwyp5g.jpg",
+    link: "/page/meta-unveils-movie-gen-rj3GtxbAQditnyIXKX6Ofw",
+    author: "elymc",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/831bd9b7-78f7-4d06-7380-44809b816500/thumbnail",
+  },
+  {
+    title: "Tokyo Expands Underground Cavern",
+    description:
+      "Tokyo is expanding its underground flood defense system, known as the 'Cathedral' complex, to combat the increasing threat of extreme weather events fueled by climate change. As reported by Reuters, the city is launching a seven-year, $250 million initiative to upgrade its infrastructure, including reinforcing levees, improving drainage systems, and constructing new reservoirs to handle more intense rainfall and prevent flooding in densely populated areas.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_thumbnail/v1728011540/server_uploads/iawkflht17fqvpj1bls7.jpg",
+    link: "/page/tokyo-expands-underground-cave-DEnVFIA2QuCsCRQokewASQ",
+    author: "elymc",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/831bd9b7-78f7-4d06-7380-44809b816500/thumbnail",
+  },
+  {
+    title: "The .io Ownership Battle",
+    description:
+      "The .io domain, linked to the British Indian Ocean Territory, is at the center of a contentious ownership battle involving the UK, Mauritius, and the Chagossian people, as geopolitical shifts and ethical concerns about digital colonialism emerge with the UK's decision to transfer control of the Chagos Archipelago to Mauritius.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_thumbnail/v1727987295/user_uploads/fwcqpgmox/io.jpg",
+    link: "/page/the-io-ownership-battle-disput-vau4oy5fSbuvP4D.7aQPPg",
+    author: "elymc",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/831bd9b7-78f7-4d06-7380-44809b816500/thumbnail",
+  },
+  {
+    title: "WWW Foundation Shut Down",
+    description:
+      "According to recent reports, the World Wide Web Foundation, founded by Sir Tim Berners-Lee in 2009 to promote an open and accessible internet, has announced its closure after 15 years of operation, citing the achievement of its primary mission as global internet access has reached nearly 70%.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_thumbnail/v1727981322/getty_uploads/1164279583.jpg",
+    link: "/page/www-foundation-shut-down-E.iziOROSCG__ZDItbY6Cg",
+    author: "elymc",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/831bd9b7-78f7-4d06-7380-44809b816500/thumbnail",
+  },
+  {
+    title: "31-year Golden Owl Hunt",
+    description:
+      "After 31 years, the legendary French treasure hunt for the Golden Owl has finally concluded, as reported by BBC News. On October 3, 2024, it was announced that the buried replica of the golden owl statuette had been discovered, bringing an end to one of the world's longest-running scavenger hunts.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_limit/v1727980811/server_uploads/uzcoepdk5vvrq2tf74ha.jpg",
+    link: "/page/31-year-golden-owl-hunt-Qa8XIhcoTjOONFF5HiH0lg",
+    author: "elymc",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/831bd9b7-78f7-4d06-7380-44809b816500/thumbnail",
+  },
+  {
+    title: "OpenAI Raises $6.6B",
+    description:
+      "OpenAI, the company behind ChatGPT, has secured a historic $6.6 billion funding round, valuing the artificial intelligence firm at $157 billion and solidifying its position as one of the world's most valuable startups, as reported by multiple sources.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_thumbnail/v1727773772/getty_uploads/2174798150.jpg",
+    link: "/page/openai-raises-6-6b-ofVMnsDdRw.cUWz28MxjBA",
+    author: "elymc",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/831bd9b7-78f7-4d06-7380-44809b816500/thumbnail",
+  },
+  {
+    title: "FTC's Amazon Suit Partially Dismissed",
+    description:
+      "U.S. District Judge John Chun has partially dismissed the FTC's antitrust lawsuit against Amazon, allowing core federal allegations to proceed while dismissing some state-level claims; the ruling, which remains sealed, divides the case into addressing alleged violations and proposed remedies, with an October 14 deadline for redaction agreements.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_thumbnail/v1727909810/server_uploads/rvgzobtvi7kcztbxi2p7.jpg",
+    link: "/page/ftcs-amazon-suit-partially-dis-H8mL0rltQCq1mYrk0gfoNg",
+    author: "elymc",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/831bd9b7-78f7-4d06-7380-44809b816500/thumbnail",
+  },
+  {
+    title: "California Bans Legacy Admissions",
+    description:
+      "California has taken a bold step in higher education reform by banning legacy and donor admissions preferences at private colleges and universities. According to reports from Fortune and NBC Bay Area, Governor Gavin Newsom signed legislation prohibiting these practices, making California the second state after Maryland to extend such a ban to private institutions.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_thumbnail/v1727909306/getty_uploads/2033128343.jpg",
+    link: "/page/california-bans-legacy-admissi-k548cPL5QhO1xm6TsumgVQ",
+    author: "elymc",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/831bd9b7-78f7-4d06-7380-44809b816500/thumbnail",
+  },
+  {
+    title: "The .io Ownership Battle",
+    description:
+      "The .io domain, linked to the British Indian Ocean Territory, is at the center of a contentious ownership battle involving the UK, Mauritius, and the Chagossian people, as geopolitical shifts and ethical concerns about digital colonialism emerge with the UK's decision to transfer control of the Chagos Archipelago to Mauritius.",
+    imageUrl:
+      "https://pplx-res.cloudinary.com/image/upload/t_thumbnail/v1727987295/user_uploads/fwcqpgmox/io.jpg",
+    link: "/page/the-io-ownership-battle-disput-vau4oy5fSbuvP4D.7aQPPg",
+    author: "elymc",
+    authorImage:
+      "https://imagedelivery.net/MPdwyYSWT8IY7lxgN3x3Uw/831bd9b7-78f7-4d06-7380-44809b816500/thumbnail",
+  },
+];
