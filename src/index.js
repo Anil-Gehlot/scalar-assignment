ccb3dcdd9573e1cbfe677d0fb3e40a6fd35c2513
anil-gehlot <anilgehlot2000@gmail.com>
2024-10-08 15:03:12 +0530

    Top most component

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import CardsContainer from "./components/CardsContainer";
+import CardDetails from "./components/CardDetails";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/discover" replace />, // Redirect root path to /discover
+  },
+  {
+    path: "/discover",
+    element: <App />,
+    children: [
+      {
+        path: "/discover",
+        element: <CardsContainer />,
+      },
+      {
+        path: "page/:cardId",
+        element: <CardDetails />,
+      },
+    ],
+  },
+]);
+
+root.render(<RouterProvider router={appRouter} />);
+
